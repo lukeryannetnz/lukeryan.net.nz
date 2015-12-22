91b9152d4081501abee7ea45897bde25fa332156
Luke Ryan <hello@lukeryan.net.nz>
2015-12-22 21:34:20 +1300

    Deploy script ignores .git dir

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # Uploads the contents of the current directory to the production S3 bucket. 
 # Requires the AWS CLI configured with AWS credentials. See https://aws.amazon.com/cli/
 
-aws s3 sync . s3://lukeryan.net.nz --region ap-southeast-2 
+aws s3 sync . s3://lukeryan.net.nz --region ap-southeast-2 --exclude ".git/*"
